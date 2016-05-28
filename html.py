6d6dcc5ec6f64800002334876fe5529b3c242bd0
Simon Forman <sforman@hushmail.com>
2016-05-28 16:11:44 -0700

    HTML and a server.

---
+++ html.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2016 Simon Forman
+#
+#    This file is part of MemeStreamer.
+#
+#    MemeStreamer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MemeStreamer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with MemeStreamer.  If not, see <http://www.gnu.org/licenses/>.
+#
+'''
+Some HTML and HTTP support stuff.
+
+The interface of the HTML generation class is pretty directly based on
+https://pypi.python.org/pypi/html but it uses ElementTree to render the
+HTML output.
+'''
+from random import randrange
+from xml.etree.ElementTree import Element, SubElement, tostringlist
+
+
+def posting(environ):
+  return environ['REQUEST_METHOD'] == 'POST'
+
+
+def start(start_response, message, mime_type):
+  start_response(message, [('Content-type', mime_type)])
+
+
+def err500(start_response, message):
+  start(start_response, '500 Internal Server Error', 'text/plain')
+  return [str(message)]
+
+
+def ok200(start_response, response):
+  start(start_response, '200 OK', 'text/html')
+  return response
+
+
+class HTML(object):
+
+  def __init__(self, element=None):
+    if element is None:
+      element = Element('html')
+    assert isinstance(element, Element), repr(element)
+    self.root = self.element = element
+
+  def __getattr__(self, tag):
+    e = HTML(SubElement(self.element, tag))
+    e.root = self.root
+    return e
+
+  def __iadd__(self, other):
+    return self._append(self.element, other)
+
+  def _append(self, to, other):
+    if isinstance(other, basestring):
+      if len(to):
+        last = to[-1]
+        if last.tail is None:
+            last.tail = other
+        else:
+            last.tail += other
+      elif to.text is None:
+        to.text = other
+      else:
+        to.text += other
+    elif isinstance(other, Element):
+      to.append(other)
+    elif isinstance(other, HTML):
+      if other.root is self.root:
+        raise ValueError('What are you doing? No recursive HTML.')
+      to.append(other.element)
+    else:
+      raise ValueError('Must only add strings or Elements not %r'
+                       % (other,))
+    return self
+
+  def __call__(self, *content, **kw):
+    for it in content:
+      self._append(self.element, it)
+    self.element.attrib.update((k.rstrip('_'), v) for k, v in kw.iteritems())
+    return self
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, exc_type, exc_value, exc_tb):
+    pass
+
+  def __repr__(self):
+    return '<HTML:%r 0x%x>' % (self.element, id(self))
+
+  def _stringify(self, encoding='us-ascii'):
+    return tostringlist(self.element, method='html', encoding=encoding)
+
+  def __str__(self):
+    return ''.join(self._stringify())
+
+  def __unicode__(self):
+    return u''.join(self._stringify('UTF-8'))
+
+  def __iter__(self):
+    return iter(self._stringify())
+
+
+def fake_out_caching(form):
+  form.input(type_='hidden', name='fake_out_caching', value=str(randrange(2**32)))
+
+
+def labeled_field(form, label, type_, name, value, **kw):
+  form.label(label, for_=name)
+  form.input(type_=type_, name=name, value=value, **kw)
+
+
+def labeled_textarea(form, label, name, value, **kw):
+  form.label(label, for_=name)
+  form.br
+  form.textarea(value, name=name, **kw)
+
---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    Copyright © 2016 Simon Forman
+#
+#    This file is part of MemeStreamer.
+#
+#    MemeStreamer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MemeStreamer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with MemeStreamer.  If not, see <http://www.gnu.org/licenses/>.
+#
+from argparse import ArgumentParser
+from os import remove, rename
+from os.path import exists, realpath
+from wsgiref.simple_server import make_server
+from server import Server
+
+
+def main(argv=None):
+  args = get_args(argv)
+  server = Server()
+  run(server, args.host, args.port)
+
+
+def make_argparser():
+  parser = ArgumentParser(
+    prog='MemeStreamer',
+    description='Run the MemeStreamer server.',
+    )
+  parser.add_argument(
+    '--host',
+    type=str,
+    help='The host (IP) to bind. Default: localhost.',
+    default='',
+    )
+  parser.add_argument(
+    '--port',
+    type=int,
+    help='The port number on which to listen. Default: 8000.',
+    default=8000,
+    )
+  return parser
+
+
+def get_args(argv=None):
+  if argv is None:
+    import sys
+    argv = sys.argv[1:]
+  return make_argparser().parse_args(argv)
+
+
+def run(app, host='', port=8000):
+  httpd = make_server(host, port, app)
+  _print_serving(host, port)
+  try:
+    httpd.serve_forever()
+  except KeyboardInterrupt:
+    pass
+
+
+def _print_serving(host, port):
+  print (
+    'server at http://%s:%i/00000000/00000000'
+    % (host or 'localhost', port)
+    )
