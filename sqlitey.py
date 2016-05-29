115e875fbae1ba2668ff69b8f10840881b6c6f82
Simon Forman <sforman@hushmail.com>
2016-05-29 10:41:40 -0700

    Comment out memcache, use SQLite.

---
+++ sqlitey.py
@@ -0,0 +1,81 @@
+from time import time
+from os.path import exists, expanduser
+import sqlite3
+
+
+SQLITE_DB = expanduser('~/memestreamer.sqlite')
+MAKE_TABLES = not exists(SQLITE_DB)
+
+
+def get_conn(db=':memory:', make_tables=False):
+  conn = sqlite3.connect(db)
+  conn.row_factory = VisibleRow
+  if make_tables:
+    print 'creating tables'
+    create_tables(conn)
+  return conn
+
+
+CREATE_TABLES = '''\
+create table bumps (when_ INTEGER, key TEXT PRIMARY KEY, from_ TEXT, what TEXT, to_ TEXT)
+create table tags (when_ INTEGER, tag TEXT PRIMARY KEY, url TEXT)
+'''.splitlines(False)
+
+
+def create_tables(conn):
+  c = conn.cursor()
+  for statement in CREATE_TABLES:
+    c.execute(statement)
+  conn.commit()
+  c.close()
+
+
+def write_tag(c, when, tag, url, **kw):
+  return insert(c, 'insert into tags values (?, ?, ?)', when, tag, url)
+
+
+def get_tag(c, tag):
+  tag = (tag,)
+  c.execute('select url from tags where tag=?', tag)
+  result = c.fetchone()
+  return str(result[0]) if result else None
+
+
+def bump(c, when, from_, what, to, **kw):
+  key = '%s:%s' % (what, to)
+  return insert(
+    c,
+    'insert into bumps values (?, ?, ?, ?, ?)',
+    when, key, from_, what, to,
+    )
+
+
+def extract_graph(c, tag):
+  c.execute('select when_, from_, to_ FROM bumps WHERE what=?', (tag,))
+  nodes, links = set(), []
+  for when, from_, to in c.fetchall():
+    nodes.update((from_, to))
+    links.append((when, from_, to))
+  return list(nodes), links
+
+
+def insert(c, query, *values):
+  try:
+    c.execute(query, values)
+  except sqlite3.IntegrityError:
+    return
+  return c.lastrowid
+
+
+class VisibleRow(sqlite3.Row):
+  def __str__(self):
+    return str(tuple(self))
+  __repr__ = __str__
+
+
+def T():
+  return int(round(time(), 3) * 1000)
+
+
+conn = get_conn(SQLITE_DB, MAKE_TABLES)
+c = conn.cursor()
--- stores.py
+++ stores.py
@@ -1,25 +1,74 @@
-from memcache import Client
+#from memcache import Client
+import logging
+from urlparse import urlparse
+from sqlitey import get_tag, write_tag, get_conn, SQLITE_DB, MAKE_TABLES, T
 from tagly import tag_for
 
 
-U2T = Client(['127.0.0.1:11213'], debug=True)
-T2U = Client(['127.0.0.1:11214'], debug=True)
+log = logging.getLogger('mon')
 
 
-def url2tag(url):
-  url = str(url)
-  tag = U2T.get(url)
-  if not tag:
-    tag = tag_for(url)
-    U2T.set(url, tag)
-    T2U.set(tag, url)
+#U2T = Client(['127.0.0.1:11213'], debug=True)
+#T2U = Client(['127.0.0.1:11214'], debug=True)
+conn = get_conn(SQLITE_DB, MAKE_TABLES)
+
+
+def url2tag(url_):
+  url = normalize_url(url_)
+  if not url:
+    log.debug('Not normalized URL %r', url_)
+    return
+
+  c, t, tag, result = conn.cursor(), T(), tag_for(url), None
+  try:
+    result = write_tag(c, t, tag, url)
+  finally:
+    c.close()
+
+  if result:
+    conn.commit()
+    log.debug('Wrote to db %s %s', tag, url)
+  else:
+    log.debug('Already in db %s %s', tag, url)
+
   return tag
 
 
 def tag2url(tag):
-  tag = str(tag)
-  url = T2U.get(tag)
-  if not url:
-    raise KeyError(tag)
-  return url
+  c = conn.cursor()
+  url = get_tag(c, tag)
+  if url:
+    log.debug('Found in DB %s %s', tag, url)
+    return url
+  log.debug('Missed in DB %s', tag)
+  raise KeyError(tag)
 
+
+##def url2tag(url):
+##  url = str(url)
+##  tag = U2T.get(url)
+##  if not tag:
+##    tag = tag_for(url)
+##    U2T.set(url, tag)
+##    T2U.set(tag, url)
+##  return tag
+
+
+##def tag2url(tag):
+##  tag = str(tag)
+##  url = T2U.get(tag)
+##  if not url:
+##    raise KeyError(tag)
+##  return url
+
+def normalize_url(url):
+  try:
+    url = str(url)
+    url = url.lower()
+    result = urlparse(url)
+  except:
+    log.exception('While parsing URL %r', url)
+    return
+  if (result.scheme in ('http', 'https')
+      and not (result.params or result.query or result.fragment)):
+    return result.geturl()
